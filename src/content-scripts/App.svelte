4cf66e999569488189fadd991ff0ced1bc0733b0
ciricc <facevk.02@mail.ru>
2022-03-12 20:53:48 +0300

    Фикс. Расширение ломало некоторые сайты

--- src/content-scripts/App.svelte
+++ src/content-scripts/App.svelte
@@ -126,40 +126,46 @@
 <div bind:this={appContainer} class="dark">
   {#if $settings}
     {#if isKinopoisk}
-      <div class={visibleFullScreenController && !visible ? "visible" : "invisible"}>
-        <div class="pr-6 py-6 screenfixed" on:click={() => show()}>
-          <div class="stroke-true-gray-50 text-shadow-dark-900 text-shadow-md w-25 h-25 items-center justify-center cursor-pointer hover:opacity-75 flex text-true-gray-50 transform transition-transform,opacity duration-100 {slideDownFullScreenController ? "translate-y-0 opacity-100" : "-translate-y-10 opacity-0 pointer-events-none"}">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-            </svg>
-          </div>
-        </div>
-      </div>
-      <div class={visible ? "visible" : "invisible"}>
-        <div class="pr-4 w-156 py-6 screenfixed" on:mouseenter={() => deleteNonActiveTimeoutHide()} on:mouseleave={() => createNonActiveTimeoutHide()}>
-          <div class="{slideDown ? "translate-y-0 opacity-100" : "-translate-y-10 opacity-0 pointer-events-none"} widget dark:bg-dark-800 dark:text-gray-300">
-            <div class="flex pt-6">
-              <Thumbler id="enabled" bind:checked={$settings.doublesubs_enabled} label="Двойные субтитры"/>
-            </div>
-            <div class="flex">
-              <Thumbler id="black_background_enabled" bind:checked={$settings.black_background_enabled} label="Черный фон субтитров"/>
+      {#if visibleFullScreenController && !visible}
+        <div>
+          <div class="pr-6 py-6 screenfixed" on:click={() => show()}>
+            <div class="stroke-true-gray-50 text-shadow-dark-900 text-shadow-md w-25 h-25 items-center justify-center cursor-pointer hover:opacity-75 flex text-true-gray-50 transform transition-transform,opacity duration-100 {slideDownFullScreenController ? "translate-y-0 opacity-100" : "-translate-y-10 opacity-0 pointer-events-none"}">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+              </svg>
             </div>
           </div>
         </div>
-      </div>
+      {/if}
+      {#if visible}
+        <div>
+          <div class="pr-4 w-156 py-6 screenfixed" on:mouseenter={() => deleteNonActiveTimeoutHide()} on:mouseleave={() => createNonActiveTimeoutHide()}>
+            <div class="{slideDown ? "translate-y-0 opacity-100" : "-translate-y-10 opacity-0 pointer-events-none"} widget dark:bg-dark-800 dark:text-gray-300">
+              <div class="flex pt-6">
+                <Thumbler id="enabled" bind:checked={$settings.doublesubs_enabled} label="Двойные субтитры"/>
+              </div>
+              <div class="flex">
+                <Thumbler id="black_background_enabled" bind:checked={$settings.black_background_enabled} label="Черный фон субтитров"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      {/if}
     {:else}
-      <div class={visible ? "visible" : "invisible"}>
-        <div class="pr-4 w-156 py-6 screenfixed" on:mouseenter={() => deleteNonActiveTimeoutHide()} on:mouseleave={() => createNonActiveTimeoutHide()}>
-          <div class="{slideDown ? "translate-y-0 opacity-100" : "-translate-y-10 opacity-0 pointer-events-none"} widget dark:bg-dark-800 dark:text-gray-300 px-6 text-base pt-4">
-            <p>
-              Управление расширением доступно только на странице с видеоплеером Кинопоиск HD.
-            </p>
-            <div class="mt-4">
-              <a href="https://hd.kinopoisk.ru" target="_blank" rel="noreferrer noopener" class="button">Перейти к просмотру кино</a>
+      {#if visible}
+        <div>
+          <div class="pr-4 w-156 py-6 screenfixed" on:mouseenter={() => deleteNonActiveTimeoutHide()} on:mouseleave={() => createNonActiveTimeoutHide()}>
+            <div class="{slideDown ? "translate-y-0 opacity-100" : "-translate-y-10 opacity-0 pointer-events-none"} widget dark:bg-dark-800 dark:text-gray-300 px-6 text-base pt-4">
+              <p>
+                Управление расширением доступно только на странице с видеоплеером Кинопоиск HD.
+              </p>
+              <div class="mt-4">
+                <a href="https://hd.kinopoisk.ru" target="_blank" rel="noreferrer noopener" class="button">Перейти к просмотру кино</a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      {/if}
     {/if}
   {/if}
 </div>
