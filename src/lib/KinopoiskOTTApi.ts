c53230e88073bda967e0ad19f37664c97bb39c83
ciricc <facevk.02@mail.ru>
2023-02-26 19:16:03 +0000

    refactor: лишний код

--- src/lib/KinopoiskOTTApi.ts
+++ src/lib/KinopoiskOTTApi.ts
@@ -8,7 +8,7 @@ export const call = async (path:string) => {
 }
 
 export const getWatchParams = async (filmId:string) => {
-  return call("watch-params/" + filmId + "?serviceId=25&extension=true");
+  return call("watch-params/" + filmId + "?serviceId=25");
 }
 
 export const getContentMetadata = async (filmId:string) => {
@@ -18,8 +18,6 @@ export const getContentMetadata = async (filmId:string) => {
 export const getContentChildren = async (filmId:string) => {
   return call("content/" + filmId + "/children?serviceId=25");
 }
-
-// https://api.ott.kinopoisk.ru/v12/hd/content/4429fcf33ca51366b42e06e86a25c917/streams?serviceId=25
 
 export const getContentStreamsMetadata = async (contentId:string) => {
   return call("content/" + contentId + "/streams/metadata?serviceId=25");
